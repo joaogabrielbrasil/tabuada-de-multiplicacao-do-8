433adcf68f852ea552777272f15309c7aef9bdb5
Joao Gabriel <jgbrl743@gmail.com>
2021-10-06 19:57:16 -0300

    Tabuada de Multiplicacao do 8 , em HTML5 , CSS3 e JavaScript

---
+++ js/script.js
@@ -0,0 +1,64 @@
+function multiplicar() {
+    var tn1 = window.document.querySelector('#txt1')
+    var n1 = Number(tn1.value)
+    var multiplic1 = 8 * n1
+    var resposta1 = window.document.querySelector('#resultado1')
+    resposta1.innerHTML = multiplic1
+
+    var tn2 = window.document.querySelector('#txt2')
+    var n2 = Number(tn2.value)
+    var multiplic2 = 8 * n2
+    var resposta2 = window.document.querySelector('#resultado2')
+    resposta2.innerHTML = multiplic2
+
+    var tn3 = window.document.querySelector('#txt3')
+    var n3 = Number(tn3.value)
+    var multiplic3 = 8 * n3
+    var resposta3 = window.document.querySelector('#resultado3')
+    resposta3.innerHTML = multiplic3
+
+    var tn4 = window.document.querySelector('#txt4')
+    var n4 = Number(tn4.value)
+    var multiplic4 = 8 * n4
+    var resposta4 = window.document.querySelector('#resultado4')
+    resposta4.innerHTML = multiplic4
+
+    var tn5 = window.document.querySelector('#txt5')
+    var n5 = Number(tn5.value)
+    var multiplic5 = 8 * n5
+    var resposta5 = window.document.querySelector('#resultado5')
+    resposta5.innerHTML = multiplic5
+
+    var tn6 = window.document.querySelector('#txt6')
+    var n6 = Number(tn2.value)
+    var multiplic6 = 8 * n6
+    var resposta6 = window.document.querySelector('#resultado6')
+    resposta6.innerHTML = multiplic6
+
+    var tn7 = window.document.querySelector('#txt7')
+    var n7 = Number(tn2.value)
+    var multiplic7 = 8 * n7
+    var resposta7 = window.document.querySelector('#resultado7')
+    resposta7.innerHTML = multiplic7
+
+    var tn8 = window.document.querySelector('#txt8')
+    var n8 = Number(tn8.value)
+    var multiplic8 = 8 * n8
+    var resposta8 = window.document.querySelector('#resultado8')
+    resposta8.innerHTML = multiplic8
+
+    var tn9 = window.document.querySelector('#txt9')
+    var n9 = Number(tn9.value)
+    var multiplic9 = 8 * n9
+    var resposta9 = window.document.querySelector('#resultado9')
+    resposta9.innerHTML = multiplic9
+
+    var tn10 = window.document.querySelector('#txt10')
+    var n10 = Number(tn10.value)
+    var multiplic10 = 8 * n10
+    var resposta10 = window.document.querySelector('#resultado10')
+    resposta10.innerHTML = multiplic10
+}
+
+
+
